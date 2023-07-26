79ff3578018b0de979f02e54fc4bf7a87060276c
sai.krishna <sai.krishna@IQGLAPIQE164.iqgateway.com>
2023-07-26 12:20:47 +0530

    commit done by krishna

---
+++ demogit/src/main/java/training/iqgateway/demogit/App.java
@@ -0,0 +1,13 @@
+package training.iqgateway.demogit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
